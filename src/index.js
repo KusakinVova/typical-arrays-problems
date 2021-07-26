d958c3d62b969345da949712cfd9ac878a0e3bef
Vladimir <kusakinvova@gmail.com>
2021-07-26 10:42:59 +0400

    feat: write func min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  
+  let min = '';
+  for (let index = 0; index < array.length; index++) {
+    if(min > array[index]) min = array[index];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+
+  let max = '';
+  for (let index = 0; index < array.length; index++) {
+    if(max < array[index]) max = array[index];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  
+  const count = array.length;
+  let sum = 0;
+  for (let index = 0; index < array.length; index++) {
+    sum += array[index];
+  }
+  return sum/count;
 }
